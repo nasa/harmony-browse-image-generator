92a303ea15812c1e86047e753efcfafc6f4df4ed
Matt Savoie <matthew.savoie@colorado.edu>
2024-04-17 11:19:25 -0600

    DAS-2108: fix typing nit for real this time.

--- harmony_browse_image_generator/color_utility.py
+++ harmony_browse_image_generator/color_utility.py
@@ -31,7 +31,7 @@ NODATA_RGBA = (0, 0, 0, 0)
 NODATA_IDX = 255
 
 
-def remove_alpha(raster: nd.ndarray) -> tuple[nd.ndarray, nd.ndarray, None]:
+def remove_alpha(raster: np.ndarray) -> tuple[np.ndarray, np.ndarray, None]:
     """remove alpha layer when it exists."""
     if raster.shape[0] == 4:
         return raster[0:3, :, :], raster[3, :, :]
